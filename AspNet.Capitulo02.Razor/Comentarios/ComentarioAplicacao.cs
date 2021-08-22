e7164e08778960164e8aa508e6b7dbdf7edb5b5a
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-08-22 17:34:02 -0300

    Gravação e listagem do Comentario.txt

---
+++ AspNet.Capitulo02.Razor/Comentarios/ComentarioAplicacao.cs
@@ -0,0 +1,42 @@
+﻿using AspNet.Capitulo02.Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace AspNet.Capitulo02.Razor.Comentarios
+{
+    public class ComentarioAplicacao
+    {
+        private readonly string caminho = AppDomain.CurrentDomain.BaseDirectory +
+            ConfigurationManager.AppSettings["caminhoArquivoComentario"];
+
+        public void Incluir(string nome, string comentario)
+        {
+            File.AppendAllText(caminho, $"{nome};{comentario}" + Environment.NewLine);
+        }
+
+        public List<ComentarioViewModel> Selecionar()
+        {
+            var comentarios = new List<ComentarioViewModel>();
+
+            foreach (var linha in File.ReadAllLines(caminho))
+            {
+                if (linha.Trim() == string.Empty)
+                {
+                    continue;
+                }
+
+                var propriedades = linha.Split(';');
+
+                var comentario = new ComentarioViewModel();
+                comentario.Nome = propriedades[0];
+                comentario.Conteudo = propriedades[1];
+
+                comentarios.Add(comentario);
+            }
+
+            return comentarios;
+        }
+    }
+}
---
+++ AspNet.Capitulo02.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNet.Capitulo02.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public string Nome { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
