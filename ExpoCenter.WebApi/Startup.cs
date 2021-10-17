9a861f6f7a5ba2278327e485ad45a2b4366d54c6
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-10-17 18:35:00 -0300

    PagamentosController

--- ExpoCenter.WebApi/Startup.cs
+++ ExpoCenter.WebApi/Startup.cs
@@ -1,9 +1,11 @@
+using ExpoCenter.Repositorios.SqlServer;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +39,12 @@ namespace ExpoCenter.WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExpoCenter.WebApi", Version = "v1" });
             });
+
+            services.AddDbContext<ExpoCenterDbContext>(
+                options => options
+                    //.UseLazyLoadingProxies()
+                    .UseSqlServer(
+                        Configuration.GetConnectionString("ExpoCenterConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
