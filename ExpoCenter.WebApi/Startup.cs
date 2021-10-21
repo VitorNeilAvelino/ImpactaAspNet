45631e3fcb53bad153fa82e9d3a60b9a727ba256
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-10-20 22:35:13 -0300

    Botão para incluir o token na tela do Swagger

--- ExpoCenter.WebApi/Startup.cs
+++ ExpoCenter.WebApi/Startup.cs
@@ -40,9 +40,35 @@ namespace ExpoCenter.WebApi
             //.AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
 
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExpoCenter.WebApi", Version = "v1" });
+
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "bearerAuth"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
             });
 
             services.AddDbContext<ExpoCenterDbContext>(
