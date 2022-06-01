a87151700937188323ff3e9b0b14f4bc4cb4080b
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-06-01 14:13:23 -0300

    UK para Número do Cartão

--- GatewayPagamento.Repositorios.SqlServer.CodeFirst/ModelConfiguration/CartaoConfiguration.cs
+++ GatewayPagamento.Repositorios.SqlServer.CodeFirst/ModelConfiguration/CartaoConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using GatewayPagamento.Dominio.Entidades;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration;
 
 namespace GatewayPagamento.Repositorios.SqlServer.CodeFirst.ModelConfiguration
@@ -9,7 +11,8 @@ namespace GatewayPagamento.Repositorios.SqlServer.CodeFirst.ModelConfiguration
         {
             Property(c => c.Numero)
                 .IsRequired()
-                .HasMaxLength(16);
+                .HasMaxLength(16)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("UKNumeroCartao") { IsUnique = true }));
 
             Property(p => p.Limite)
                 .IsRequired();
